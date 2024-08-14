9165fc2d41c386f9e52e177e04c542f28494dce1
Subashh-Chaudhary <chysubash15963@gmail.com>
2024-08-15 01:21:13 +0545

    quiz question

---
+++ quiz.py
@@ -0,0 +1,20 @@
+# x = int(input())
+# y = int(input())
+ 
+# x = x / y
+# y = y / x
+ 
+# print(y)
+
+# x = int(input())
+# y = int(input())
+ 
+# x = x % y
+# x = x % y
+# y = y % x
+ 
+# print(y)
+
+x = 1 / 2 + 3 // 3 + 4 ** 2
+print(x)
+print(type(1/1))
