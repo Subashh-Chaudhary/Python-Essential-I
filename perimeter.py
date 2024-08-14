4756f7c2805e8fd3e93253a7f85039d179abcb9f
Subashh-Chaudhary <chysubash15963@gmail.com>
2024-08-15 00:56:16 +0545

    typecasting in python

---
+++ perimeter.py
@@ -0,0 +1,3 @@
+length = float(input("Please enter the length of rectangle : "))
+breadth = float(input("Please enter the breadth of rectangle : "))
+print("The peremeter of rectangle is ", (2 * (length + breadth)))
