b69f5a06d5b8369f4e28e01e89beae3eb9c71f88
Subashh-Chaudhary <chysubash15963@gmail.com>
2024-08-17 20:43:49 +0545

    looping in for

---
+++ for.py
@@ -0,0 +1,12 @@
+print("with Single paramater")
+for i in range(10):
+          print("The value of i : ", i)
+
+
+print("with Two parameters")
+for i in range(5, 15):
+        print("The value of i : ", i)
+
+print("with Three parameters")
+for i in range(0, 101, 20):
+        print("The value of i : ", i)
